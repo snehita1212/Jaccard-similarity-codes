cbbbe51a04c7e11a7ba11fd6de1d3584a9f71168
snehita1212 <92868475+snehita1212@users.noreply.github.com>
2024-05-22 18:41:00 +0530

    Add files via upload

---
+++ compare_jaccard_mashmap.py
@@ -0,0 +1,43 @@
+razers_file = "mashmap.out"
+
+dictionary = {}
+with open(razers_file, "r") as file:
+    for line in file:
+        if line.startswith("@"):
+            continue  # Skip headers
+        fields = line.split("\t")
+        read_id = fields[0]
+        read_razers = read_id
+        flag = fields[4]
+        # length=int(fields[2])
+        # ref_name = fields[2]
+        position = int(fields[7])
+        # strand = '-' if (flag & 16) else '+'
+        if flag == '+':
+            dictionary["@" + read_id] = position
+
+count = 0
+
+jaccard_file = "output.sam"
+with open(jaccard_file, "r") as file:
+    next(file)
+    next(file)
+    for line in file:
+        fields = line.split("\t")
+        read_id = fields[0] #.split("\t")
+        read_id = read_id
+        read_jaccard = read_id
+        flag = int(fields[1])
+        length = int(fields[2])
+        position = int(fields[4])
+        if read_id in dictionary:
+            pos = dictionary[read_id]
+            if position >= (pos - length) and position <= (pos + length):
+                count = count + 1
+
+percent = (count / len(dictionary)) * 100
+print(f"Accuracy: {percent:.2f}%")
+print(count)
+
+print(read_razers)
+print(read_jaccard)
